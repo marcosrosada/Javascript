cad017013d71557c1b9e333057d0b58e2752818d
Marcos Rosada <ochild@gmail.com>
2016-02-14 21:04:45 -0200

    Add twice functions. Declaration and Anonymous

--- functions.js
+++ functions.js
@@ -1,4 +1,22 @@
 // Function Declaration
 
+console.log("Function Declaration");
 
-// Function Expression / Anonymous  
+function sun(param1, param2) {
+	return param1 + param2;
+}
+
+console.log(sun);
+console.log(sun(10, 15));
+
+
+// Function Expression / Anonymous
+
+console.log("Function Expression / Anonymous");
+
+var sun = function(param1, param2){
+	return param1 + param2;
+}
+
+console.log(sun);
+console.log(sun(10, 15));
