7a1d2b83afbdb920bdfe8f5a2200bb9b5049c2d6
Marcos Rosada <ochild@gmail.com>
2016-02-15 01:29:22 -0200

    Call External function from inside of the Object

--- functionObject.js
+++ functionObject.js
@@ -9,3 +9,22 @@ var person = {
 console.log(person);
 console.log(person.getAge);
 console.log(person.getAge());
+
+
+
+
+// Another way to call a function inside of the object
+var getAge = function() {
+	return this.age;
+};
+
+var person2 = {
+	name : "John",
+	age	: 25,
+	getAge : getAge
+}
+
+console.log("=============================================");
+console.log(person);
+console.log(person.getAge);
+console.log(person.getAge());
