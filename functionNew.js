37ef9789db0fc6ba0c9b142254c77448469eb06e
Marcos Rosada <ochild@gmail.com>
2016-02-16 17:07:22 -0200

    Add Factory function and Function Contructor

---
+++ functionNew.js
@@ -0,0 +1,32 @@
+// Factory Function
+console.log("Factory Function");
+
+var createPerson = function(name, age){
+	return {
+		name : name,
+		age : age
+	};
+};
+
+console.log(createPerson("John", 25));
+console.log(createPerson("Mark", 28));
+
+
+// Function Contructor
+console.log("Function Contructor");
+
+var Person = function(name, age){
+	this.name = name;
+	this.age  = age;
+};
+
+console.log(new Person("John", 25));
+console.log(new Person("Mark", 28));
+
+var peter = {};
+Person.call(peter, "Peter", 18);
+console.log(peter);
+
+var maria = {};
+Person.call(maria, "Maria", 32);
+console.log(maria);
