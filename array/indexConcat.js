d268f42eb8a068888a05e221230c1a505ea9e32f
Marcos Rosada <ochild@gmail.com>
2016-02-18 00:26:19 -0200

    Adding .concat function

---
+++ array/indexConcat.js
@@ -0,0 +1,13 @@
+var country = ["USA", "Germany", "Brazil"];
+var city = ["New York", "Berlin", "Rio de Janeiro"];
+
+var result = country.concat(city);
+
+console.log("Result:", result);
+
+/*[ 'USA',
+  'Germany',
+  'Brazil',
+  'New York',
+  'Berlin',
+  'Rio de Janeiro' ]*/
