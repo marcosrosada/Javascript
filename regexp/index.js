3bfbbed7c37ee5395c8d2475770aa21da3a6f024
Marcos Rosada <ochild@gmail.com>
2016-02-19 12:04:12 -0200

    Hifen

--- regexp/index.js
+++ regexp/index.js
@@ -5,7 +5,7 @@ console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
 
-// Add \ to escape special charset
+// \ to escape special charset
 regExp	= /\(48\) 9999-9999/;
 phone 	= "(48) 9999-9999";
 
@@ -13,7 +13,7 @@ console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
 
-// Add ^ to Start and $ end
+// ^ to Start and $ end
 regExp	= /^\(48\) 9999-9999$/;
 phone 	= "(48) 9999-9999";
 
@@ -21,7 +21,7 @@ console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
 
-// Add Group
+// Group
 regExp	= /^\([0-9][0-9]\) [0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$/;
 phone 	= "(48) 9876-1234";
 
@@ -29,7 +29,7 @@ console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
 
-// Add Multiple
+// Multiple
 regExp	= /^\([0-9]{2}\) [0-9]{4}-[0-9]{4}$/;
 phone 	= "(48) 9876-1234";
 
@@ -37,9 +37,17 @@ console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
 
-// Add Multiple with limit
+// Multiple with limit
 regExp	= /^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/;
 phone 	= "(48) 99876-1234";
+
+console.log("\nEXEC:", regExp.exec(phone));
+console.log("TEST:", regExp.test(phone));
+
+
+// hifen
+regExp	= /^\([0-9]{2}\) [0-9]{4,5}-?[0-9]{4}$/;
+phone 	= "(48) 998761234";
 
 console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
