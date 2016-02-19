1c0eb12ce115f8d1668f3fe5e3826a557bf58c99
Marcos Rosada <ochild@gmail.com>
2016-02-19 11:54:52 -0200

    Multiple with limit!

--- regexp/index.js
+++ regexp/index.js
@@ -21,9 +21,25 @@ console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
 
-// Add ^ to Start and $ end
+// Add Group
+regExp	= /^\([0-9][0-9]\) [0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$/;
+phone 	= "(48) 9876-1234";
+
+console.log("\nEXEC:", regExp.exec(phone));
+console.log("TEST:", regExp.test(phone));
+
+
+// Add Multiple
 regExp	= /^\([0-9]{2}\) [0-9]{4}-[0-9]{4}$/;
 phone 	= "(48) 9876-1234";
+
+console.log("\nEXEC:", regExp.exec(phone));
+console.log("TEST:", regExp.test(phone));
+
+
+// Add Multiple with limit
+regExp	= /^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/;
+phone 	= "(48) 99876-1234";
 
 console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
