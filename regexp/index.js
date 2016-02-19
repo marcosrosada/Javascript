833d50adfd1f9b9cfad06efc9d66231e1df599ce
Marcos Rosada <ochild@gmail.com>
2016-02-19 11:49:47 -0200

    Adding group

--- regexp/index.js
+++ regexp/index.js
@@ -4,6 +4,7 @@ var phone  = "9999-9999";
 console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
+
 // Add \ to escape special charset
 regExp	= /\(48\) 9999-9999/;
 phone 	= "(48) 9999-9999";
@@ -11,9 +12,18 @@ phone 	= "(48) 9999-9999";
 console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
 
+
 // Add ^ to Start and $ end
 regExp	= /^\(48\) 9999-9999$/;
 phone 	= "(48) 9999-9999";
+
+console.log("\nEXEC:", regExp.exec(phone));
+console.log("TEST:", regExp.test(phone));
+
+
+// Add ^ to Start and $ end
+regExp	= /^\([0-9][0-9]\) [0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$/;
+phone 	= "(48) 9876-1234";
 
 console.log("\nEXEC:", regExp.exec(phone));
 console.log("TEST:", regExp.test(phone));
