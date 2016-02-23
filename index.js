44897d8655ccad6cdc700810ab324230539f057f
Marcos Rosada <ochild@gmail.com>
2016-02-22 23:55:21 -0300

    Try Catch Finally

---
+++ index.js
@@ -0,0 +1,40 @@
+var ClassError = function (message){
+	this.message = message;
+	this.name = "ClassError";
+};
+
+var callSomething = function (value){
+
+	try { 
+        if (value == "")  
+        	throw new ClassError("Value is Empty!");
+
+        if (isNaN(value) && typeof value === "string") 
+        	throw new ClassError("Value is a isNaN!");
+
+        if (typeof value === "object") 
+        	throw new ClassError("Value is a Object!");
+        
+        if (value >= 10)
+        	throw new ClassError("Value is too high!");
+        else
+        	throw new ClassError("Value is too low!");
+        
+        if (!value)    	  
+        	throw new ClassError("Value is undefined");
+	}
+    catch(err) {
+        console.log("Catch: [ " + value + " ] " + err.message);
+    }
+    finally {
+        console.log("Finally clean up something\n");
+    }
+}
+
+
+callSomething("");
+callSomething("Lorem ipsum dolor sit amet ...");
+callSomething(15);
+callSomething(4);
+callSomething({ name: "John", age: 30});
+callSomething();
