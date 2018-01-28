865134cec02cc4fc724e1da0232c6cea947704db
Marcos Rosada <ochild@gmail.com>
2018-01-28 17:29:49 -0200

    Closures

---
+++ functions/closure/closure5.js
@@ -0,0 +1,32 @@
+var makeCounter = function () {
+    var privateCounter = 0;
+
+    function changeBy(val) {
+        privateCounter += val;
+    }
+    return {
+        increment: function () {
+            changeBy(1);
+        },
+        decrement: function () {
+            changeBy(-1);
+        },
+        value: function () {
+            return privateCounter;
+        }
+    }
+};
+
+var Counter1 = makeCounter();
+var Counter2 = makeCounter();
+
+console.log( Counter1.value() ); /* Console 0 */
+
+Counter1.increment();
+Counter1.increment();
+console.log( Counter1.value() ); /* Console 2 */
+
+Counter1.decrement();
+console.log( Counter1.value() ); /* Console 1 */
+
+console.log( Counter2.value() ); /* Console 0 */
