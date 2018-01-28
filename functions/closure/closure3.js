1020ed90b11c99350790a4509a2d919f13e54562
Marcos Rosada <ochild@gmail.com>
2018-01-28 17:45:36 -0200

    Closures

--- functions/closure/closure3.js
+++ functions/closure/closure3.js
@@ -7,5 +7,18 @@ function doAdd(x) {
 var onAdd5 = doAdd(5);
 var onAdd10 = doAdd(10);
 
+console.log( doAdd(3)(3) ); // 6
 console.log( onAdd5(2) ); // 7
 console.log( onAdd10(2) ); // 12
+
+// ---- 
+var doCounter = (function () {
+    var counter = 0;
+    return function () {
+        return counter += 1;
+    }
+})();
+
+console.log( doCounter() );
+console.log( doCounter() );
+console.log( doCounter() );
